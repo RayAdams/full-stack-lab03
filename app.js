db6330f910189c6d493d216b1d02b1ec7107febf
RayAdams <cramerrachael@gmail.com>
2017-06-01 20:55:20 -0400

    working stage

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
 var friends = ["Hadlea", "Dom", "Sara", "Priya", "Pepper"];
 
+//creates button and calls global function to display the friends song
+var btn = document.getElementById('my-btn');
+btn.addEventListener("click", function() {
+    for(var i = 0; i < friends.length; i++){
+        sing(friends[i]);
+    };
+});
+
+
 function sing(friend){
-    console.log(friend.toUpperCase() + ":");
-    for(var i = 99; i >= 2; i--){
-       console.log(i + " lines of code in the file, " + i + " lines of code; " + friend + " strikes one out, clears it all out, " + (i - 1) + " lines of code in the file.");
-       } while(i === 1){
-            console.log(i + " line of code in the file, " + i + " line of code; " + friend + " strikes one out, clears it all out, no more lines of code in the file.");
-            i = 0;
+    //creates div to hold text
+    var friendDiv = document.createElement('div');    
+    friendDiv.className = 'friend';
+
+    //creates Name of friend to title their chorus
+    var name = document.createTextNode(friend.toUpperCase() + ":")
+    friendDiv.appendChild(name);
+
+    //loops through friend array
+    for(var i = 99; i > 0; i--) {
+        if (i > 1) {
+            //creates paragraghs inside the div and adds current text lyric
+            var para = document.createElement('p');
+            var text = document.createTextNode(i + " lines of code in the file, " + i + " lines of code; " + friend + " strikes one out, clears it all out, " + (i - 1) + " lines of code in the file.");
+            //adds the elements to the page once eventListener is met
+            para.appendChild(text)
+            friendDiv.appendChild(para);
         }
+        else  {
+            var para = document.createElement('p');
+            var text = document.createTextNode(i + " line of code in the file, " + i + " line of code; " + friend + " strikes one out, clears it all out, no more lines of code in the file.");      
+            para.appendChild(text)
+            friendDiv.appendChild(para);
+         }  
+    } 
+    
+    //adds each div to the body once once everything runs
+    document.body.appendChild(friendDiv);
 };
 
-for(var i = 0; i < friends.length; i++){
-    console.log(sing(friends[i]));
-};
-
-//alt way to complete
-// var friends = ["Hadlea", "Dom", "Sara", "Priya", "Pepper"];
-
-// for(var i = 0; i < friends.length; i++){
-//     console.log(friends[i].toUpperCase() + ':');
-//     for(var j = 99; j > 0; j--){
-//          if(j > 2){
-            //     console.log(j + 'song lyrics....');
-            // } else if(j ===2 ){
-            //     console.log(j + 'song lyrics...');
-            // } else {
-            //     console.log(j + 'song lyric...')
-            // }        
-
-//     }
-// };
